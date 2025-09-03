6f02dfb17c21423731042be35119213ce40599e9
namira <namiraq336@gmail.com>
2025-09-04 01:16:53 +0530

    added about page

--- src/landing_page/about/Team.js
+++ src/landing_page/about/Team.js
@@ -2,7 +2,39 @@ import React from 'react'
 
 const Team = () => {
   return (
-    <div>Team</div>
+    <div className='container'>
+      <div className='row p-3 mt-5 mb-5 border-top'>
+        <h1 className='text-center '>People</h1>
+      </div>
+
+      <div className='row p-3 text-muted' style={{fontSize:"1.2em",lineHeight:"1.8"}}>
+
+        <div className='col-6 p-3 text-center'>
+
+          <img src="media/images/nithinKamath.jpg" alt="" style={{borderRadius:"100%",width:"60%"}} />
+
+          <h4 className='mt-5'>Nithin Kamath</h4>
+          <h6>Founder & CEO</h6>
+
+        </div>
+        <div className='col-6 p-3'>
+           <p>
+            Nithin bootstrapped and founded Zerodha in 2010 to overcome the
+            hurdles he faced during his decade long stint as a trader. Today,
+            Zerodha has changed the landscape of the Indian broking industry.
+          </p>
+          <p>
+            He is a member of the SEBI Secondary Market Advisory Committee
+            (SMAC) and the Market Data Advisory Committee (MDAC).
+          </p>
+          <p>Playing basketball is his zen.</p>
+          <p>
+            Connect on <a href="" className='text-decoration-none'>Homepage</a> / <a href="" className='text-decoration-none'>TradingQnA</a> /{" "}
+            <a href="" className='text-decoration-none'>Twitter</a>
+          </p>
+        </div>
+      </div>
+    </div>
   )
 }
 
